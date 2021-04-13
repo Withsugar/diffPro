7071c9c013503f643c3ea252be517f1ae0a67823
1303279512@qq.com <1303279512@qq.com>
2021-04-13 15:01:29 +0800

    firstCommit

---
+++ src/main/java/com/hetao/lmy/MessageBuilder.java
@@ -0,0 +1,29 @@
+package com.hetao.lmy;
+
+public class MessageBuilder {
+
+    public static final String PROVIDED = "Please provide a name!";
+    public static final String HELLO = "Hello ";
+
+    public static String getMessage(String name) {
+        StringBuilder result = new StringBuilder();
+        if ("".equals(name)||null==name) {
+            result.append(PROVIDED);
+        } else if(HELLO.equals(name)){
+            result.append("This is a hello!");
+        }else {
+            result.append(HELLO).append(name);
+        }
+        return result.toString();
+    }
+
+    public static int getMulRet(int a,int b) {
+
+        if(a>0&&b>0){
+            return a*b;
+        }
+        return 0;
+
+    }
+
+}
---
+++ src/test/java/com/hetao/lmy/MessageBuilderTest.java
@@ -0,0 +1,46 @@
+package com.hetao.lmy;
+
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test
+public class MessageBuilderTest {
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        System.out.println("开始class之前");
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        System.out.println("结束class之后");
+    }
+
+    @Test
+    public void testNameMessage() {
+        String name = "Jake";
+        MessageBuilder messageBuilder = new MessageBuilder();
+        Assert.assertEquals(MessageBuilder.HELLO + name, messageBuilder.getMessage(name));
+    }
+
+    @Test
+    public void testEmptyMessage() {
+        String name="";
+        MessageBuilder messageBuilder = new MessageBuilder();
+        Assert.assertEquals(MessageBuilder.PROVIDED, messageBuilder.getMessage(name.trim()));
+    }
+
+    @Test
+    public void testNullMessage() {
+        String name=null;
+        MessageBuilder messageBuilder = new MessageBuilder();
+        Assert.assertEquals(MessageBuilder.PROVIDED, messageBuilder.getMessage(name));
+    }
+
+
+
+
+}
